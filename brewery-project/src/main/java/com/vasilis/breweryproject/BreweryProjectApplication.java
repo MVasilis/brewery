54e482270409af325d16a156816307aee198e543
Vasilis <vasilis.martos@gmail.com>
2020-11-02 16:53:05 +0200

    first project commit

---
+++ brewery-project/src/main/java/com/vasilis/breweryproject/BreweryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.vasilis.breweryproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreweryProjectApplication.class, args);
+	}
+
+}
---
+++ brewery-project/src/test/java/com/vasilis/breweryproject/BreweryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasilis.breweryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
